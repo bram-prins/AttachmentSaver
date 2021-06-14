b63793af04490dc1c7ad01ddcba6f0a9d6342a2b
Bram Prins <prins@ciratum.nl>
2021-06-14 14:32:38 +0200

    Add project files.

---
+++ AttachmentSaver/OpslagProfiel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttachmentSaver
+{
+    public class OpslagProfiel
+    {
+        public string Name { get; set; }
+        public string Sender { get; set; }
+        public string FilenameFilter { get; set; }
+        public List<string> FileExtensions { get; set; }
+        public string FileLocation { get; set; }
+    }
+}
